6a6ed39d08301d2871c6d68dc1b12c9851548fe9
Amalichristina.Y <Amalichristina.Y@EXIND3020-47.allfon.in>
2022-04-07 11:14:52 +0530

    First commit

---
+++ Quickstart/src/com/training/Function.java
@@ -0,0 +1,13 @@
+package com.training;
+
+public class Function {
+
+	
+		// TODO Auto-generated method stub
+		public static int getInfo() {
+			return(1);
+		}
+
+	
+
+}
---
+++ Quickstart/src/com/training/Greeting.java
@@ -0,0 +1,13 @@
+package com.training;
+
+ class Greeting {
+	public                                                          String getMessage() {
+		return "Welcome to Java Programming";
+	}
+	public static String getInfo() {
+		return "Hello From Static Method";
+	}
+
+}
+
+
