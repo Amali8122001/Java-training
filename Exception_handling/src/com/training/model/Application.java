fe6a6eb32151790f567a405d1338355f29ec82b2
Amalichristina.Y <Amalichristina.Y@EXIND3020-47.allfon.in>
2022-04-08 10:20:13 +0530

    amali

---
+++ Exception_handling/src/com/training/model/Application.java
@@ -0,0 +1,15 @@
+package com.training.model;
+
+import com.task9.Customer;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Customer cust = new Customer(1098 , "Suresh" , 638154892 , "amalichristina@gmail.com");
+				
+		
+
+	}
+
+}
