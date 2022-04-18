d74a405ebdb6ef5afdb31c52068a5a98d275d7e0
Amalichristina.Y <Amalichristina.Y@EXIND3020-47.allfon.in>
2022-04-18 15:04:44 +0530

    hii

---
+++ Maven-2Project/src/main/java/com/model/training/CreditCardService.java
@@ -0,0 +1,29 @@
+package com.model.training;
+
+public class CreditCardService {
+	
+	public boolean addCard(CreditCard[] card) {
+		
+		
+	
+			
+			
+		
+		
+		return false;
+		
+		
+	}
+	
+	 public CreditCard[] getCards() {
+		
+		 
+		 return null;
+		 
+		 
+		 
+	 }
+	
+	
+
+}
