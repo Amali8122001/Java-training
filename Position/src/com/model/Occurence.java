a43ace7d5ca78358ce9ece7a2e9b4b829ba88a73
Amalichristina.Y <Amalichristina.Y@EXIND3020-47.allfon.in>
2022-04-28 13:04:28 +0530

    hh

---
+++ Position/src/com/model/Occurence.java
@@ -0,0 +1,23 @@
+package com.model;
+
+import java.util.Scanner;
+
+public class Occurence {
+	
+	private static Scanner scan;
+
+	public Occurence() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public static Scanner getScan() {
+		return scan;
+	}
+
+	public static void setScan(Scanner scan) {
+		Occurence.scan = scan;
+	}
+	
+
+}
